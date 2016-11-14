dc55cad46343557ae0a7e9902e9bf093c335766d
Oleg Sergienko <O.Petrovich.S@yandex.ua>
2016-11-14 16:05:32 +0200

    Modified form and other

--- app/js/main.js
+++ app/js/main.js
@@ -5,4 +5,29 @@ $(document).ready(function() {
   $('.popup').magnificPopup({type:'image'});
   $('.popup_content').magnificPopup();
 
+  $('#contactForm').validate({
+    rules: {
+      email: {
+        required: true,
+        email: true
+      },
+      name: {
+        required: true,
+        minlength: 3
+      }
+    },
+    messages: {
+      email: {
+        required: 'This field "Email" is required'
+      },
+      name: {
+        required: 'This field "Name" is required',
+        minlength: 'The name must be more than 2 characters'
+      }
+    },
+    submitHandler: function() {
+      alert('valid');
+      // form.submit();
+    }
+  });
 });
